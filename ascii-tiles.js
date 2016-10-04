75a0ca5a30d188d1a94b2718401b21074deb211b
Chris Martens <chrisamaphone@gmail.com>
2016-10-04 08:48:17 -0400

    basic functionality

--- ascii-tiles.js
+++ ascii-tiles.js
@@ -34,23 +34,27 @@ window.onload = function() {
 
 var generateButton = document.getElementById("generateButton");
 generateButton.addEventListener("click", function () {
+  source = document.getElementById('predicates').value;
   var generated = generate(); 
-  document.getElementById("generated").innerHTML = generated;
+  document.getElementById("generated").innerHTML = "<pre>"+generated+"</pre>";
 });
 
-document.getElementById('source').innerHTML = source;
-document.getElementById('fileinput').addEventListener('change', readFileAndDisplay, false);
+document.getElementById('predicates').innerHTML = source;
+// document.getElementById('fileinput').addEventListener('change', readFileAndDisplay, false);
 }
 
 // Utils
 
 function tokenize(s) {
-  var csv = s.split(/\)\,\s*/);
+  // var csv = s.split(/\)\,\s*/);
+  var preds = s.split(" ");
   var tokens = [];
-  for (var i = 0; i < csv.length; i++) {
-    var pred_args = csv[i].split(/\s*\(\s*/);
+  for (var i = 0; i < preds.length; i++) {
+    var pred_args = preds[i].split(/\s*\(|\)\s*/);
     var pred = pred_args[0];
-    var args = pred_args[1].split(/\s*\,\s*/);
+    if (pred_args[1] != undefined) {
+      var args = pred_args[1].split(/\s*\,\s*/);
+    } else {args = [] }
 
     tokens.push({pred: pred, args: args});
   }
@@ -58,14 +62,22 @@ function tokenize(s) {
 }
 
 function atoms_to_tiles(atoms) {
+  var max_x = 0;
+  var max_y = 0;
   var m = [];
   for (var i = 0; i < atoms.length; i++) {
     var p = atoms[i].pred;
-    if (p = "at") {
+    if (p == "at") {
       var args = atoms[i].args;
-      var x = args[0];
-      var y = args[1];
+      var x = parseInt(args[0], 10);
+      var y = parseInt(args[1], 10);
+      // console.log("x= "+x+"; y = "+y);
+      if(x > max_x) { max_x = x; }
+      if(y > max_y) { max_y = y; }
       var entity = args[2];
+      if(entity == undefined) {
+        console.log("Undefined entity at "+x+", "+y+"!");
+      }
       if(m[x] == undefined){
         var xrow = [];
         xrow[y] = entity;
@@ -75,20 +87,21 @@ function atoms_to_tiles(atoms) {
       }
     }
   }
-  return m;
+  console.log("Width: "+(max_x+1)+", Height: "+(max_y+1));
+  return {map:m, width:max_x+1, height:max_y+1};
 }
 
 
-var source = "at(0,0,player), at(0,1,floor), at(0,2,water),\
-              at(1,0,key), at(1,1,floor),at(1,2,door),\
-              at(2,0,floor), at(2,1,floor),at(2,2,water)"
+var source = "at(0,0,player) at(0,1,floor) at(0,2,water) at(1,0,key) at(1,1,floor) at(1,2,door) at(2,0,floor) at(2,1,floor) at(2,2,water)"
 
 
 token_table = 
 { "player": "@", 
   "floor":".", 
   "key":"*", 
-  "door":"]"
+  "door":"]",
+  "water":"#",
+  "lock":"x"
 }
 
 // expects m[i][j] = "token" where "token" is a key in the table
@@ -98,7 +111,12 @@ function draw_map(m, width, height) {
   for(var i=0; i<height; i++) {
     for(var j=0; j<width; j++) {
       if(m[i] != undefined && m[i][j] != undefined){
-        mapstring += token_table[m[i][j]];
+        var entity = m[i][j];
+        var ch = token_table[entity];
+        mapstring += ch;
+        if(ch == undefined) {
+          console.log("Unable to find a char for "+entity);
+        }
       } else {
         mapstring += " "
       }
@@ -112,6 +130,12 @@ function draw_map(m, width, height) {
 /* Generate something same size as the source, starting from the same word
  * as the source. */
 function generate () {
+
+  var atoms = tokenize(source);
+  var {map, width, height} = atoms_to_tiles(atoms);
+  var map_string = draw_map(map, width, height);
+
+  return map_string;
 
 }
 
