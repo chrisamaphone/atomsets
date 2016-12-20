4068a10ab1fd7d0edc4ba1d5ec10e08b0064a11c
Chris Martens <chrisamaphone@gmail.com>
2016-12-20 17:54:31 -0500

    add image drawing to canvas code

--- canvas-tiles/canvas-tiles.js
+++ canvas-tiles/canvas-tiles.js
@@ -10,19 +10,59 @@ function randomColor() {
   return "rgb(" + r + "," + g + "," + b + ")";
 }
 
+var tilegrid = [];
+var entities = ["cat_l", "dagger", "heart", "wall"]
+
+function random_int(min, max) {
+  var x = Math.floor(Math.random()*(max-min+1)) + min;
+  return x;
+}
+
+function random_element(arr) {
+  var x = arr[random_int(0, arr.length-1)];
+  return x;
+}
+
+function initialize_random(width, height) {
+  tilegrid = [];
+  for(var y = 0; y < height; y++) {
+    tilegrid.push([]);
+    for(var x = 0; x < width; x++) {
+      tilegrid[y].push([]);
+      var entity = random_element(entities);
+      tilegrid[y][x].push(entity);
+    }
+  }
+}
+
 function draw(canvas) {
 
   var ctx = canvas.getContext('2d');
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-  var width = canvas.width/tilesize;
-  var height = canvas.height/tilesize;
+  var height = tilegrid.length;
+  var width = 0;
+  if (height > 0) { width = tilegrid[0].length; }
 
+  /* fill with random colors */
   for (var y = 0; y < height; y++) {
     for(var x = 0; x < width; x++) {
       ctx.fillStyle = randomColor();
       ctx.fillRect(x*tilesize,y*tilesize,tilesize,tilesize);
     }
   }
+
+  /* draw what's in tilegrid*/
+  for (var y = 0; y < height; y++) {
+    for(var x = 0; x < width; x++) {
+      for(var entity_index = 0; entity_index < tilegrid[y][x].length; entity_index++) {
+        var entity_name = tilegrid[y][x][entity_index];
+        var img = document.getElementById(entity_name);
+        ctx.drawImage(img, x*tilesize, y*tilesize, tilesize, tilesize); 
+      }
+    }
+  }
+  
 
 }
 
@@ -62,6 +102,9 @@ var canvas = document.getElementById('map');
 var generateButton = document.getElementById("generateButton");
 
 generateButton.addEventListener("click", function () {
+    var width = canvas.width/tilesize;
+    var height = canvas.height/tilesize;
+    initialize_random(width, height);
     draw(canvas);
 });
 
